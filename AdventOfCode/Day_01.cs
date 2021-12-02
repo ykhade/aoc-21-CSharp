4cfedbe26e10ee4b2a2ccfbcb199e0c4e0a6ff83
Yash Khade <ykhade@nevada.unr.edu>
2021-12-01 23:39:25 -0800

    Solved Day Part1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace AdventOfCode;
@@ -10,9 +14,24 @@ public class Day_01 : BaseDay
     public Day_01()
     {
         _input = File.ReadAllText(InputFilePath);
+
+        int Increment()
+        {
+            int[] input = { };
+            int increaseAmount = 0;
+            input = Array.ConvertAll (File.ReadAllLines (InputFilePath), x => int.Parse (x));
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] < input[i + 1])
+                    increaseAmount++;
+            }
+            return increaseAmount;
+        }
+        
+        Console.WriteLine(Increment());
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new("Solution 1 1616");
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
